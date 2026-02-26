7ad791bb2b775903d8bf6346100032281f0c0998
John Jameson <jjameson@princeton.edu>
2026-02-25 21:06:42 -0500

    footer

--- assets/theme.js
+++ assets/theme.js
@@ -32,9 +32,9 @@
                 Getting Started
               </a>
               <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="../drupal">Drupal CSA Module</a></li>
-                <!--<li><a class="dropdown-item" href="#">WordPress plugin</a></li>-->
-                <li><a class="dropdown-item" href="https://editoria11y.princeton.edu/install" title="External link">Library configuration <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-square" viewBox="0 0 16 16">
+                <li><a class="dropdown-item" href="../install">Installation</a></li>
+                <li><a class="dropdown-item" href="../drupal">Drupal Module</a></li>
+                <li><a class="dropdown-item" href="https://wordpress.org/plugins/editoria11y-accessibility-checker/" title="External link">WordPress Plugin <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="14" fill="currentColor" class="bi bi-arrow-right-square" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
 </svg></a></li>
     `
@@ -61,14 +61,6 @@
     };
     window.location.replace(`/${goTo}/about`);
   }
-
-  /* Footer **********************/
-
-  const footer = document.createElement('footer');
-  footer.innerHTML = `
-      &copy; Editoria11y 2026`;
-  footer.setAttribute('class', 'container col-lg-12 mx-auto pt-5 my-5 text-body-secondary border-top');
-  document.body.appendChild(footer);
 
   /* On this page **********************/
 
